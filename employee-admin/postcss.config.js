30a8334b5b8c61314d51008652a409df923d8aa7
Aliziwe Qeqe <st10382076@vcconnect.edu.za>
2025-11-07 21:18:43 +0200

    Ui is perfect

---
+++ employee-admin/postcss.config.js
@@ -0,0 +1,8 @@
+// postcss.config.js or .mjs
+export default {
+  plugins: {
+    "@tailwindcss/postcss": {},
+    // optionally autoprefixer if needed
+  }
+};
+
--- employee-admin/vite.config.js
+++ employee-admin/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import fs from 'fs'
-
+import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   //we then set the server
   server: {
     //https settings
